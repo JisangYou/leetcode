529b43a7ee02fd18f2b77f42df8fd79ea539f5e9
Jay-You <31605792+JisangYou@users.noreply.github.com>
2022-06-04 01:58:04 +0900

    Time: 228 ms (51.11%), Space: 37.1 MB (8.89%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun dominantIndex(nums: IntArray): Int {
+        val tempArr = nums.sortedArray()
+        var result = -1
+
+        if (nums.count() <= 1) {
+            return 0
+        }
+
+        if (tempArr[tempArr.lastIndex] 
+            >= tempArr[tempArr.lastIndex - 1].times(2)) {
+            nums.mapIndexed { index, i ->
+                if (i == tempArr[tempArr.lastIndex]) {
+                    result = index
+                }
+            }
+        }
+        return result
+    }
+}
