738fa6ea371f3046ab786f5b2ea014ad03bf3dd8
JayYoo <31605792+JisangYou@users.noreply.github.com>
2022-09-19 01:25:03 +0900

    Time: 482 ms, Memory: 64.7 MB - LeetHub

--- reverse-string/reverse-string.kt
+++ reverse-string/reverse-string.kt
@@ -1,5 +1,14 @@
 class Solution {
     fun reverseString(s: CharArray): Unit {
-        s.reverse()
+        // s.reverse()
+        val midPoint = (s.size / 2) - 1
+        if (midPoint < 0) return
+        var reverseIndex = s.lastIndex
+        for (index in 0..midPoint) {
+            val tmp = s[index]
+            s[index] = s[reverseIndex]
+            s[reverseIndex] = tmp
+            reverseIndex--
+        }
     }
 }
