dd3aeefb12d5203c48035059d1fe5c4b65543529
JayYoo <31605792+JisangYou@users.noreply.github.com>
2022-11-11 19:06:12 +0900

    Time: 482 ms (44.81%), Space: 57.5 MB (56.40%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.kt
+++ 0200-number-of-islands/0200-number-of-islands.kt
@@ -1,39 +1,36 @@
 class Solution {
     fun numIslands(grid: Array<CharArray>): Int {
-        if (grid.isEmpty()) return 0
+       if (grid.isEmpty()) return 0
 
-        var count = 0
-        val row: Int = grid.count()
-        val col: Int = grid[0].count()
+    var count = 0
+    val row = grid.count()
+    val col = grid[0].count()
 
-        //Move in Four Directions
+    val directions = arrayOf(intArrayOf(1, 0), intArrayOf(0, 1), intArrayOf(-1, 0), intArrayOf(0, -1))
+    val qu: Queue<IntArray> = LinkedList()
 
-        //Move in Four Directions
-        val directions = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
-        val qu: Queue<IntArray> = LinkedList()
+    for (i in 0 until row) {
+        for (j in 0 until col) {
+            if (grid[i][j] == '1') {
+                count++
+                qu.add(intArrayOf(i, j))
 
-        for (i in 0 until row) {
-            for (j in 0 until col) {
-                if (grid[i][j] == '1') {
-                    count++
-                    qu.add(intArrayOf(i, j))
-
-                    //Mark as visited
-                    grid[i][j] = '2'
-                    while (!qu.isEmpty()) {
-                        val curr = qu.poll()
-                        for (dir in directions) {
-                            val r = curr[0] + dir[0]
-                            val c = curr[1] + dir[1]
-                            if (r in 0 until row && c >= 0 && c < col && grid[r][c] == '1') {
-                                qu.add(intArrayOf(r, c))
-                                grid[r][c] = '2'
-                            }
+                //Mark as visited
+                grid[i][j] = '2'
+                while (qu.isEmpty().not()) {
+                    val curr = qu.poll()
+                    for (dir in directions) {
+                        val r = curr[0] + dir[0]
+                        val c = curr[1] + dir[1]
+                        if (r >= 0 && c >= 0 && r < row && c < col && grid[r][c] == '1') {
+                            qu.add(intArrayOf(r, c))
+                            grid[r][c] = '2'
                         }
                     }
                 }
             }
         }
+    }
 
     return count
     }
