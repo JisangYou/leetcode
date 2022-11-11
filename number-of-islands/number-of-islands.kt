e3cec52f2aebc507ad82a050b8df76879c5e2eeb
JayYoo <31605792+JisangYou@users.noreply.github.com>
2022-11-11 18:37:47 +0900

    Time: 433 ms, Memory: 62.6 MB - LeetHub

--- number-of-islands/number-of-islands.kt
+++ number-of-islands/number-of-islands.kt
@@ -1,56 +1,29 @@
 class Solution {
-    fun numIslands(islandGrid: Array<CharArray>): Int {
-        val h = islandGrid.size
-        if (h == 0) return 0
-        val l: Int = islandGrid[0].size
-        var islands = 0
-        val visited = Array(islandGrid.size) {
-            BooleanArray(
-                islandGrid[0].size
-            )
-        }
-
-        // 초기화
-        for (i in 0 until h) {
-            for (j in 0 until l) {
-                visited[i][j] = false
-            }
-        }
-        // bfs 시작
-        val queue: Queue<String> = LinkedList()
-        for (i in 0 until h) {
-            for (j in 0 until l) {
-                if (!visited[i][j] && islandGrid[i][j] == '1') {
-                    queue.add("$i,$j")
-                    BFS(queue, islandGrid, visited)
-                    islands++
+    fun numIslands(grid: Array<CharArray>): Int {
+        var count = 0
+        for (i in grid.indices) {
+            for (j in 0 until grid[i].count()) {
+                if (grid[i][j] == '1') {
+                    mark(grid, i, j)
+                    count++
                 }
             }
         }
-        return islands
+        return count
     }
-    
-    fun BFS(queue: Queue<String>, islandGrid: Array<CharArray>, visited: Array<BooleanArray>) {
-        val H = islandGrid.size
-        val L: Int = islandGrid[0].size
-        while (!queue.isEmpty()) {
-            val x = queue.remove()
-            val row = x.split(",".toRegex()).toTypedArray()[0].toInt()
-            val col = x.split(",".toRegex()).toTypedArray()[1].toInt()
-            if (row < 0 || 
-                col < 0 || 
-                row >= H || 
-                col >= L || 
-                visited[row][col] || 
-                islandGrid[row][col] != '1') {
-                //nothing to do 
-            } else {
-                visited[row][col] = true
-                queue.add(row.toString() + "," + (col - 1)) //go left
-                queue.add(row.toString() + "," + (col + 1)) //go right
-                queue.add((row - 1).toString() + "," + col) //go up
-                queue.add((row + 1).toString() + "," + col) //go down
-            } 
+    private fun mark(grid: Array<CharArray>, row: Int, col: Int) {
+        grid[row][col] = '-'
+        if (row < grid.count() - 1 && grid[row + 1][col] == '1') {
+            mark(grid, row + 1, col) // 상
+        }
+        if (row > 0 && grid[row - 1][col] == '1') {
+            mark(grid, row - 1, col) // 하
+        }
+        if (col > 0 && grid[row][col - 1] == '1') {
+            mark(grid, row, col - 1) // 좌
+        }
+        if (col < grid[row].count() - 1 && grid[row][col + 1] == '1') {
+            mark(grid, row, col + 1) //우
         }
     }
 }
