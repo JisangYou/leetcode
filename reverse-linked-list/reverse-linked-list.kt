42a8c384cbdbbe6cd856c59bb098ae41b0e95f26
JayYoo <31605792+JisangYou@users.noreply.github.com>
2023-07-23 01:41:20 +0900

    Time: 155 ms, Memory: 36.5 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.kt
+++ reverse-linked-list/reverse-linked-list.kt
@@ -9,14 +9,16 @@
  */
 class Solution {
      fun reverseList(head: ListNode?): ListNode? {
-        var currentNode = head
-        var newNode: ListNode? = null 
-        while(currentNode != null) {
-            var nextNode = currentNode.next
-            currentNode.next = newNode
-            newNode = currentNode
-            currentNode = nextNode
+        var prevNode: ListNode? = null // 이전 노드를 가리키는 포인터
+        var currentNode = head // 현재 처리 중인 노드를 가리키는 포인터
+
+        while (currentNode != null) { // 연결 리스트를 끝까지 순회합니다.
+            val nextNode = currentNode.next // 다음 노드를 임시로 저장합니다.
+            currentNode.next = prevNode // 현재 노드의 다음 노드를 이전 노드로 변경하여 뒤집습니다.
+            prevNode = currentNode // 이전 노드를 현재 노드로 업데이트합니다.
+            currentNode = nextNode // 다음 노드를 현재 노드로 업데이트하여 순회를 계속합니다.
         }
-        return newNode
+
+        return prevNode // 뒤집힌 연결 리스트의 첫 번째 노드인 prevNode를 반환합니다.
     }
 }
