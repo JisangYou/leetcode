f0b48782ca970ac0f8d3ee5ff9c6dd1369ff3845
JayYoo <31605792+JisangYou@users.noreply.github.com>
2023-08-20 00:49:24 +0900

    Time: 127 ms, Memory: 33.2 MB - LeetHub

--- fibonacci-number/fibonacci-number.kt
+++ fibonacci-number/fibonacci-number.kt
@@ -1,11 +1,17 @@
 class Solution {
+    private val cache = HashMap<Int, Int>()
     fun fib(n: Int): Int {
+        if (cache.containsKey(n)) {
+            return cache[n]!!
+        }
         val result: Int
         if (n < 2) {
             result = n
         } else {
-            result = fib(n - 1) + fib(n - 2)
+            result = fib(n- 1) + fib(n - 2)
         }
+        // keep the result in cache.
+        cache[n] = result
         return result
     }
 }
